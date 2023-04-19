6de164acc0e5a9a6cb7e47017e714ac95c60c28c
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-20 01:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+function app(arr){
+	let n = arr.length
+	let f = Math.floor(n / 2)
+	arr.sort()
+	let i = 0, count = 0
+	
+	while(i < n){
+	    count = 0
+	    let j = i
+	    while(j < n){
+	        if(arr[i] == arr[j]){
+	            count++
+	            j++
+	        }
+	        else{
+	            break
+	        }
+	    }
+	    if(count > f)
+	    {
+	        break
+	    }
+	    i = j
+	}
+	return arr[i]
+}
+	
+module.exports = app;
